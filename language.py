fae08510580afefd8b71bcbd01cbefb6e2ff69f7
Colin B Hamilton <cbhsixtysix@gmail.com>
2016-05-13 21:44:29 -0400

    Create Language class, which can populate an NGramTrie from a file

---
+++ language.py
@@ -0,0 +1,70 @@
+
+from NGramTrie import NGramTrie
+import pprint
+
+
+
+class Language:
+    def __init__(self, n=3):
+        self.n_grams = NGramTrie(n)
+
+# These functions handle transforming characters before counting them.
+#   They are intended to be overwritten and customized by subclassing
+
+# The transform function is called once for each character read from the
+#   input, and should return a string, to transform the given character to.
+#   Typically this is a single character, and most of the time is the same
+#   as the character passed in.  A return value of the empty string indicates
+#   nothing being appended, ie. the character is ignored.  An extra space is
+#   added before the start and at the end of each file.
+    def transform(self, char, gram):
+        if not char.isalpha():
+            char = self.transform_space(char)
+        if char.isspace() and gram.endswith(char):
+            return ""
+        else:
+            return self.standardize(char)
+
+    def transform_space(self, char):
+        return " "
+
+    def standardize(self, char):
+        return char.upper()
+
+    def first_gram(self):
+        return " "
+
+    def last_gram(self):
+        return " "
+
+    def read_from_file(self, filename):
+        trie = self.n_grams
+        def _update(gram, char):
+            gram = gram + self.transform(char, gram)
+            gram = gram[-min(len(gram), trie.n_max):]
+            if len(gram) == trie.n_max:
+                trie.add(gram)
+            return gram
+
+        gram = ""
+        for char in self.first_gram():
+            gram = _update(gram, char)
+        with open(filename, "r") as file:
+            for line in file:
+                for char in line:
+                    gram = _update(gram, char)
+            for char in self.last_gram():
+                gram = _update(gram, char)
+        self.n_grams = trie
+
+    def __str__(self):
+        string = ""
+        grams = self.n_grams.frequencies()
+        for (gram, freq) in sorted(grams.items(), key=lambda x: x[1]):
+            string += "'" + gram + "'  " + str(freq) + "\n"
+        return string[:-1]
+
+    def compare(self, other):
+        # Get frequencies, calculate similarity
+        return 0
+
